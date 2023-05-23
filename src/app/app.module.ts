e3f76c300ed876bf2ff547931b8174f28aac1414
leonardo-ericom <leonardo@ericom.com.br>
2023-05-23 10:51:03 -0300

    Componente CHAMADO-LIST criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,6 +41,7 @@ import { ClienteCreateComponent } from './components/cliente/cliente-create/clie
 import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
 import { ClienteListComponent } from "./components/cliente/cliente-list/cliente-list.component";
 import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
+import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 
 @NgModule({
   declarations: [
@@ -57,6 +58,7 @@ import { ClienteUpdateComponent } from './components/cliente/cliente-update/clie
     ClienteDeleteComponent,
     ClienteListComponent,
     ClienteUpdateComponent,
+    ChamadoListComponent,
  
    
    
